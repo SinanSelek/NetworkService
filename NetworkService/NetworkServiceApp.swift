675d40808c1132029b8492442b50030c62a3c30b
Sinan Selek <sselek62@gmail.com>
2023-03-15 17:32:21 +0300

    Initial Commit

---
+++ NetworkService/NetworkServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkServiceApp.swift
+//  NetworkService
+//
+//  Created by Sinan Selek on 15.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
